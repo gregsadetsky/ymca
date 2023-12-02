375d4c134ff6fe40b2514f9eb328b47b31ba6937
gregsadetsky <lepetitg@gmail.com>
2023-12-02 08:22:08 -0500

    .

--- src/main.ts
+++ src/main.ts
@@ -1,69 +1,67 @@
 import "./style.css";
-import {Howl, Howler} from 'howler';
+import { Howl, Howler } from "howler";
 
 function isMobile(): boolean {
   return /iPhone|iPad|iPod|Android/i.test(navigator.userAgent);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-  if(window.location.hash === '#rctv') {
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.location.hash === "#rctv") {
     start();
     return;
   }
 
-  if(isMobile()) {
+  if (isMobile()) {
     // change font size of button to 60
-    document.querySelector('#imready')!.style.fontSize = '20px';
+    document.querySelector("#imready")!.style.fontSize = "20px";
+
+    document.querySelector("#imready")!.innerHTML +=
+      "<br/><br/><br/>(Please enable sound.)";
   }
 
-  // center on screen
-  document.querySelector('#imready')!.style.top = window.innerHeight / 2 - 50 + 'px';
-  // center, minus the width of the button
-  document.querySelector('#imready')!.style.left = window.innerWidth / 2 - (isMobile() ? 180 : 450) + 'px';
-
-  document.querySelector('#imready')!.addEventListener('click', function() {
-    start()
-  })
-
-});
-
-function start() {
-  document.querySelector('#imready')!.style.display = 'none';
-
-  const sound = new Howl({
-    src: ['/loop.wav'],
-    loop: true,
-  });
-
-  sound.play();
-
   // make the canvas fit the full window
-  const canvas = document.querySelector('canvas')!;
+  const canvas = document.querySelector("canvas")!;
   canvas.width = window.innerWidth;
   canvas.height = window.innerHeight;
+  canvas.getContext("2d").imageSmoothingEnabled = false;
 
-  // load all images
-  const images = ['M1.jpeg', 'M2.jpeg', 'M3.jpeg'];
-  // create new image elements
-  const imageElements = images.map(image => {
-    return new Promise((resolve) => {      
-      const img = document.createElement('img');
-      img.src = `/${image}`;
-      img.onload = () => {
-        resolve(img);
-      }
-    })
+  // get size of button
+  const buttonWidth = document.querySelector("#imready")!.offsetWidth;
+  const buttonHeight = document.querySelector("#imready")!.offsetHeight;
+
+  // center on screen
+  document.querySelector("#imready")!.style.top =
+    window.innerHeight / 2 - buttonHeight / 2 + "px";
+  // center, minus the width of the button
+  document.querySelector("#imready")!.style.left =
+    window.innerWidth / 2 - buttonWidth / 2 + "px";
+
+  document.querySelector("#imready")!.addEventListener("click", function () {
+    // load all images
+    const images = ["M1.jpeg", "M2.jpeg", "M3.jpeg"];
+    // create new image elements
+    const imageElements = images.map((image) => {
+      return new Promise((resolve) => {
+        const img = document.createElement("img");
+        img.src = `/${image}`;
+        img.onload = () => {
+          resolve(img);
+        };
+      });
+    });
+
+    Promise.all(imageElements).then((images) => {
+      start(images);
+    });
   });
+});
 
-  Promise.all(imageElements).then((images) => {
-    slideshow(images)
-  })
-}
+function start(images: HTMLImageElement[] = []) {
+  document.querySelector("#imready")!.style.display = "none";
 
-function slideshow(images) {
   let index = 0;
-  const canvas = document.querySelector('canvas')!;
-  const ctx = canvas.getContext('2d')!;
+  const canvas = document.querySelector("canvas")!;
+  const ctx = canvas.getContext("2d")!;
 
   function slideshowLoop() {
     index++;
@@ -73,8 +71,25 @@ function slideshow(images) {
     const randomX = Math.floor(Math.random() * 5);
     const randomY = Math.floor(Math.random() * 5);
 
-    ctx.drawImage(images[index], (112*randomX), (90*randomY), 112, 90, 0, 0, window.innerWidth, window.innerHeight);
+    ctx.drawImage(
+      images[index],
+      112 * randomX,
+      90 * randomY,
+      112,
+      90,
+      0,
+      0,
+      window.innerWidth,
+      window.innerHeight
+    );
     setTimeout(slideshowLoop, 1875);
   }
   slideshowLoop();
+
+  const sound = new Howl({
+    src: ["/loop.wav"],
+    loop: true,
+  });
+
+  sound.play();
 }
